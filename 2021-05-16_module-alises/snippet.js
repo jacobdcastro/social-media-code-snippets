ef635021dbff52f0f35f0dc6919fd6d1ea080271
Jacob D. Castro <jdcastro.business@gmail.com>
2021-05-16 16:15:05 -0700

    new post

---
+++ 2021-05-16_module-alises/snippet.js
@@ -0,0 +1,5 @@
+// with module aliases + unorganized directories
+import MyComponent from '../../../components/common/MyComponent'
+
+// with module alises + better directory structure
+import { MyComponent } from '@components/common'
